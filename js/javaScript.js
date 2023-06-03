724be73b326fc45dedfe56dad2a18a8d38ded6e0
BIS0M <bsmrc0910@gmail.com>
2023-06-03 21:16:33 +0900

    06.03

--- js/javaScript.js
+++ js/javaScript.js
@@ -3,6 +3,76 @@ $(document).ready(function () {
         $('.inmenu').slideToggle('slow ');
     });
 
-    $('.menu_home').click(function(){
+    $('#home').click(function(){
+        $('.inmenu').css({
+            "display": "none"
+        });
+        $('.menu_home').css({
+            "color": "rgb(49,49,49)",
+        });
+        $('.menu_int').css({
+            "color": "gray"
+        });
+        $('.menu_hobby').css({
+            "color": "gray"
+        });
+        $('.contents1').css({
+            "display": "inline-block"
+        });
+        $('.contents2').css({
+            "display": "none"
+        });
+        $('.contents3').css({
+            "display": "none"
+        });
+       
+    });
+
+    $('#interests').click(function(){
+        $('.inmenu').css({
+            "display": "none"
+        });
+        $('.menu_int').css({
+            "color": "rgb(49,49,49)",
+        });
+        $('.menu_home').css({
+            "color": "gray"
+        });
+        $('.menu_hobby').css({
+            "color": "gray"
+        });
+        $('.contents1').css({
+            "display": "none"
+        });
+        $('.contents2').css({
+            "display": "inline-block"
+        });
+        $('.contents3').css({
+            "display": "none"
+        });
+    });
+
+    $('#hobby').click(function(){
+        $('.inmenu').css({
+            "display": "none"
+        });
+        $('.menu_hobby').css({
+            "color": "rgb(49,49,49)",
+        });
+        $('.menu_home').css({
+            "color": "gray"
+        });
+        $('.menu_int').css({
+            "color": "gray"
+        });
+        $('.contents1').css({
+            "display": "none"
+        });
+        $('.contents2').css({
+            "display": "none"
+        });
+        $('.contents3').css({
+            "display": "inline-block"
+        });
     });
 });
